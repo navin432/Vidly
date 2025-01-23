f2e9108054cf7b1a050dc05e4628cee5db84a431
Navin <navin20604@gmail.com>
2025-01-22 20:26:57 -0500

    Applying first TDD - for the new requirement of movie return rental

---
+++ x.js
@@ -0,0 +1,13 @@
+// POST /api/returns {customerId, movieID}
+
+
+// return 401 if client is not logged in
+// return 400 if customerId is not provided
+// return 400 if movieId is not provided
+// return 404 if no rental founf for this customer/movie
+// return 400 if the rental already processed
+// return 200 if valid request
+// set the return date
+// calculate the rental fee
+// Increase the stock
+// Return the rental
